342f24083d220ff04c8d34f5d73bb74fa15be92c
muhsinking <muhsinking@gmail.com>
2015-08-12 10:27:42 -0500

    further experimentation with orbits around mouse

--- assets/js/untitled.js
+++ assets/js/untitled.js
@@ -6,17 +6,17 @@
 // 	var W = window.innerWidth, H = window.innerHeight;
 // 	canvas.width = W;
 // 	canvas.height = H;
-// 	var mouse = {};
-//
-// 	// mouse tracking
-// 	canvas.addEventListener('mousemove', track_mouse, false);
-//
-// 	function track_mouse(e)
-// 	{
-// 		// get mouse position relative to document
-// 		mouse.x = e.pageX;
-// 		mouse.y = e.pageY;
-// 	}
+	// var mouse = {};
+  //
+	// // mouse tracking
+	// canvas.addEventListener('mousemove', track_mouse, false);
+  //
+	// function track_mouse(e)
+	// {
+	// 	// get mouse position relative to document
+	// 	mouse.x = e.pageX;
+	// 	mouse.y = e.pageY;
+	// }
 //
 // 	function draw()
 // 	{
@@ -36,8 +36,22 @@
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var w = canvas.width;
-var h = canvas.height;
+
+var w = window.innerWidth, h = window.innerHeight;
+canvas.width = w;
+canvas.height = h;
+
+var mouse = {};
+
+// mouse tracking
+canvas.addEventListener('mousemove', track_mouse, false);
+
+function track_mouse(e)
+{
+  // get mouse position relative to document
+  mouse.x = e.pageX;
+  mouse.y = e.pageY;
+}
 
 var circle = function(color, r) {
     ctx.fillStyle = color;
@@ -58,17 +72,35 @@ var redraw = function() {
     ctx.fillRect(0, 0, w, h);
 
     // set origin to center
-    ctx.translate(w / 2, h / 2);
+    ctx.translate(mouse.x,mouse.y);
 
     // draw sun
     circle('yellow', 20);
 
     // rotate + move along x
-    ctx.rotate(i / 100);
+    ctx.rotate(i / 20);
     ctx.translate(100, 0);
 
     // draw planet
-    circle('green', 10);
+    circle('blue', 10);
+
+    // rotate + move along x
+    // ctx.rotate(i / 15);
+    ctx.translate(30, 0);
+
+    // draw planet
+    circle('grey', 5);
+
+    // ctx.rotate(15/i);
+
+    ctx.translate(-130, 0);
+
+    // rotate + move along x
+    ctx.rotate(i / 15);
+    ctx.translate(30, 0);
+
+    // draw planet
+    circle('grey', 5);
 
     ctx.restore();
 
